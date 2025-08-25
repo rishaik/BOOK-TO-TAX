93656a90c99c3253d6cf1b980b467232f2f5e566
rishaik <114862256+rishaik@users.noreply.github.com>
2025-08-26 00:25:05 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // accessible on LAN
-    port: 5173       // choose your port
+    host: '0.0.0.0',
+    port: 5173
   }
 });
