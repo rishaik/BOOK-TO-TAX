34bd0dc2b79988adc315bc86cd6f2837760cb151
rishaik <114862256+rishaik@users.noreply.github.com>
2025-08-26 00:27:54 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
-    port: 5173
+    host: '0.0.0.0',      // allow external access
+    port: 5173,           // or any custom port
+    allowedHosts: ['book-to-tax.onrender.com'] 
   }
 });
